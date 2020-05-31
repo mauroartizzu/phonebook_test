4a1ec4042676d61d785ff7b1f124b030ec3a8879
Mauro Artizzu <mauro.artizzu@gmail.com>
2020-05-31 21:40:42 +0200

    settings connessione database

--- app/settings.php
+++ app/settings.php
@@ -14,6 +14,12 @@ return function (ContainerBuilder $containerBuilder) {
                 'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
                 'level' => Logger::DEBUG,
             ],
+            'db' => [
+                'host' => 'db',
+                'dbname' => 'italiaonline',
+                'user' => 'italiaonline',
+                'pass' => 'password',
+            ]
         ],
     ]);
 };
