a7b6a7aa82569adc759a812abf9cb8af874c2c11
Mauro Artizzu <mauro.artizzu@gmail.com>
2020-05-31 21:42:14 +0200

    importato repository phonebook

--- app/repositories.php
+++ app/repositories.php
@@ -1,13 +1,13 @@
 <?php
 declare(strict_types=1);
 
-use App\Domain\User\UserRepository;
-use App\Infrastructure\Persistence\User\InMemoryUserRepository;
+use App\Domain\Phonebook\PhonebookRepository;
+use App\Infrastructure\Persistence\Phonebook\DatabasePhonebookRepository;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
-    // Here we map our UserRepository interface to its in memory implementation
+    // Here we map our PhonebookRepository interface to its in memory implementation
     $containerBuilder->addDefinitions([
-        UserRepository::class => \DI\autowire(InMemoryUserRepository::class),
+        PhonebookRepository::class => \DI\autowire(DatabasePhonebookRepository::class),
     ]);
 };
